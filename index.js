e7c4875f0ebbaf0a2f1969186a9f3e84a58a10c6
Ahsan Adil <muhammadmahsanadil@gmail.com>
2023-12-28 00:51:55 +0500

    file structed properly

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ function addCourse() {
   const courseNameInput = document.getElementById("courseNameInput");
   const creditHoursInput = document.getElementById("creditHoursDropdown");
   const gpaInput = document.getElementById("gpaDropdown");
-  const selectedCoursesList = document.getElementById("selectedCoursesList");
   const totalCreditHoursSpan = document.getElementById("totalCreditHours");
 
-  // Get the values entered by the user
   const courseName = courseNameInput.value;
   const creditHours = parseFloat(creditHoursInput.value);
   const gpa = parseFloat(gpaInput.value);
 
-  // Check if the inputs are valid
   if (isNaN(creditHours) || isNaN(gpa) || courseName.trim() === "") {
     alert("Invalid input");
     return;
   }
 
-  // Calculate the grade based on GPA
   let grade = "";
   if (gpa == 4) {
     grade = "A - Excellent";
@@ -32,59 +28,41 @@ function addCourse() {
     grade = "F - Fail";
   }
 
-  // Create a course object
   const course = { courseName, creditHours, gpa, grade };
 
-  // Store the course data in local storage
   if (localStorage.getItem("courses")) {
-    // If courses already exist in local storage, retrieve them and add the new course
     const storedCourses = JSON.parse(localStorage.getItem("courses"));
     storedCourses.push(course);
     localStorage.setItem("courses", JSON.stringify(storedCourses));
   } else {
-    // If no courses exist in local storage, create a new array and add the course
     localStorage.setItem("courses", JSON.stringify([course]));
   }
 
-  // Update the total credit hours
   const currentTotalCreditHours = parseFloat(totalCreditHoursSpan.textContent);
   const newTotalCreditHours = currentTotalCreditHours + creditHours;
   totalCreditHoursSpan.textContent = newTotalCreditHours.toFixed(2);
 
-  // Clear the input fields
   courseNameInput.value = "";
   creditHoursInput.value = "";
   gpaInput.value = "";
-
-  // Reload the page to display the updated course list from local storage
   location.reload();
 }
 
 function deleteCourse(courseIndex) {
-  // Retrieve the courses from local storage
   const storedCourses = JSON.parse(localStorage.getItem("courses"));
-
-  // Remove the selected course from the array
   storedCourses.splice(courseIndex, 1);
-
-  // Update the local storage with the modified array
   localStorage.setItem("courses", JSON.stringify(storedCourses));
-
-  // Reload the page to display the updated course list from local storage
   location.reload();
 }
 
-// Function to display courses on the screen
 function displayCourses() {
   const selectedCoursesList = document.getElementById("selectedCoursesList");
   const tbody = selectedCoursesList.querySelector("tbody");
-  tbody.innerHTML = ""; // Clear the existing content
+  tbody.innerHTML = "";
 
-  // Retrieve the courses from local storage
   const storedCourses = JSON.parse(localStorage.getItem("courses"));
 
   if (storedCourses) {
-    // Loop through the stored courses and display them
     storedCourses.forEach((course, index) => {
       const newRow = tbody.insertRow();
       const cell1 = newRow.insertCell(0);
@@ -98,7 +76,6 @@ function displayCourses() {
       cell3.textContent = course.gpa;
       cell4.textContent = course.grade;
 
-      // Create a delete button and set its attributes
       const deleteButton = document.createElement("button");
       deleteButton.textContent = "Delete";
       deleteButton.className = "delete-button";
@@ -110,18 +87,14 @@ function displayCourses() {
   }
 }
 
-// Call the function to display courses when the page loads
 window.onload = function () {
   displayCourses();
 };
 
-// Function to calculate the total GPA and total credit hours
 function calculateTotalGPA() {
   const totalGPASpan = document.getElementById("totalGPA");
   const totalGPAGrade = document.getElementById("totalGPAGrade");
   const totalCreditHoursSpan = document.getElementById("totalCreditHours");
-
-  // Retrieve the courses from local storage
   const storedCourses = JSON.parse(localStorage.getItem("courses"));
 
   if (storedCourses && storedCourses.length > 0) {
@@ -136,7 +109,6 @@ function calculateTotalGPA() {
     const overallGPA = totalGPA / totalCreditHours;
     totalGPASpan.textContent = overallGPA.toFixed(2);
 
-    // Calculate the grade for the total GPA
     let grade = "";
     if (overallGPA >= 4) {
       grade = "A - Excellent";
@@ -155,7 +127,6 @@ function calculateTotalGPA() {
     totalGPAGrade.textContent = grade;
     totalCreditHoursSpan.textContent = totalCreditHours.toFixed(2);
   } else {
-    // Handle the case when there are no stored courses
     totalGPASpan.textContent = "N/A";
     totalGPAGrade.textContent = "-";
     totalCreditHoursSpan.textContent = "0";
@@ -163,41 +134,28 @@ function calculateTotalGPA() {
 }
 
 function generatePDF() {
-  // Create a new jsPDF instance
   const doc = new jsPDF();
-
-  // Select the HTML content you want to convert to PDF
   const content = document.querySelector("html");
 
-  // Create a canvas element to hold the HTML content
   html2canvas(content, {
-    scale: 0.7, // Adjust the scale as needed
-    useCORS: true, // Enable cross-origin resource sharing if needed
+    scale: 0.7,
+    useCORS: true,
   }).then((canvas) => {
-    // Convert the canvas to an image
     const imgData = canvas.toDataURL("image/jpeg");
-
-    // Add the image to the PDF
     doc.addImage(imgData, "JPEG", 10, 10, 190, 0);
-
-    // Save the PDF with a specific filename (e.g., "calculated_gpa.pdf")
     doc.save("calculated_gpa.pdf");
   });
 }
-// JavaScript to toggle the dialog
 const howToUseDialog = document.getElementById("howToUseDialog");
 
 function toggleDialog() {
   if (howToUseDialog.style.display === "block") {
-    // If the dialog is currently displayed, hide it
     howToUseDialog.style.display = "none";
   } else {
-    // If the dialog is hidden, display it
     howToUseDialog.style.display = "block";
   }
 }
 
 function closeHowToUseDialog() {
-  // Close the dialog
   howToUseDialog.style.display = "none";
 }
